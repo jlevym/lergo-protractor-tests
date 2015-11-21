900988cb26fd94c1fa515d573463bec2d48411bc
Guy Mograbi <guy.mograbi@gmail.com>
2015-11-21 22:51:43 +0200

    add script to automatically remove all items

---
+++ remove_all_instances.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// script to destroy all system tests instances
+var AWS = require('aws-sdk');
+var _ = require('lodash');
+AWS.config.region = process.env.VAGRANT_REGION;
+AWS.config.update({accessKeyId: process.env.VAGRANT_AWS_KEY, secretAccessKey: process.env.VAGRANT_AWS_ACCESS_KEY });
+
+var logger = require('log4js').getLogger('remove_all_instances');
+
+var ec2 = new AWS.EC2();
+
+function removeInstances() {
+    ec2.describeInstances(function (err, data) {
+        if (err) {
+            logger.error(err);
+        } else {
+            //logger.info(JSON.stringify(data, {}, 4));
+
+            var instances = [];
+            _.each(data.Reservations, function (r) {
+                _.each(r.Instances, function (i) {
+                    var name = _.find(i.Tags, {'Key': 'Name'});
+                    //logger.info(i.InstanceId + '[' + ( name ? name.Value : 'Unnamed' )  + ']::' + i.State.Name + '.' + i.State.Code);
+                    //if (i.State.Code === 16 && name.Value === 'vagrant-system-tests'){
+                    if (name && name.Value === 'vagrant-system-tests'){
+                    instances.push(i.InstanceId);
+                    //return i;
+                    }
+                });
+            });
+
+            if (instances.length > 0) {
+                ec2.terminateInstances({InstanceIds: instances}, function (err, data) {
+                    if (err) {
+                        logger.error(err, err.stack);
+                    } // an error occurred
+                    if (data) {
+                        logger.info(data);
+                    }           // successful response
+                });
+            }
+
+
+        }
+    });
+
+}
+
+
+setTimeout( removeInstances, parseInt(process.env.VAGRANT_TERMINATE_TIMEOUT || '1',10));
