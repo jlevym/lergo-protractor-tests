4a43601cc771ff2f43fee4e93ecf939c0a90d9bf
Guy Mograbi <guy.mograbi@gmail.com>
2015-11-21 11:15:16 +0200

    add support to browserstack

--- protractor.sanity.conf.js
+++ protractor.sanity.conf.js
@@ -1,5 +1,7 @@
-
+var logger = require('log4js').getLogger('protractor.sanity.conf');
 var spec = [  'spec/sanity/**/*.spec.js'];
+
+var _ = require('lodash');
 
 
 if (!!process.env.LERGO_SPEC){
@@ -11,31 +13,64 @@ spec = ['spec/normalize.js'].concat(spec);
 console.log('spec is', spec);
 
 
+var browserName = process.env.BROWSER_NAME || process.env.BROWSER_TYPE || 'chrome';
+
+var capabilities = { browserName : browserName };
+
+if ( browserName === 'chrome'){
+    _.merge(capabilities, { 'chromeOptions': {'args': ['--disable-extensions']} });
+}
+
+if ( browserName === 'phantomjs'){
+    _.merge(capabilities,{});
+}
+
+
+if ( browserName === 'browserstack-chrome' || browserName === 'browserstack_chrome'){
+
+    if ( !process.env.BROWSERSTACK_USERNAME || !process.env.BROWSERSTACK_KEY ){
+        logger.error('browserstack credentials are missing');
+        process.exit(1);
+    }
+
+    logger.info('running browserstack on [' + process.env.BROWSERSTACK_USERNAME + ':' + process.env.BROWSERSTACK_KEY + ']');
+
+    // Input capabilities
+    _.merge(capabilities, {
+        'browserstack.user': process.env.BROWSERSTACK_USERNAME,
+        'browserstack.key': process.env.BROWSERSTACK_KEY,
+        'browserName': 'Chrome',
+        'browser_version': '46.0',
+        'browserstack.local' : 'true',
+        'os': 'Windows',
+        'os_version': '10',
+        'resolution': '1920x1080'
+    });
+}
+
+
+seleniumAddress = browserName.indexOf('browserstack') >= 0 ? 'http://hub.browserstack.com/wd/hub' : 'http://localhost:4444/wd/hub';
+
 exports.config = {
-  // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+    // The address of a running selenium server.
+    seleniumAddress: seleniumAddress,
 
-  // Spec patterns are relative to the location of this config.
-  specs: spec,
-
-
-  capabilities: {
-    'browserName': process.env.BROWSER_NAME || 'chrome',
-    'chromeOptions': {'args': ['--disable-extensions']}
-  },
+    // Spec patterns are relative to the location of this config.
+    specs: spec,
+    capabilities: capabilities,
 
 
-  // A base URL for your application under test. Calls to protractor.get()
-  // with relative paths will be prepended with this.
-  baseUrl: process.env.LERGO_ENDPOINT || 'http://localhost:1616',
-  //baseUrl: 'http://realstaging.lergodev.info',
+    // A base URL for your application under test. Calls to protractor.get()
+    // with relative paths will be prepended with this.
+    baseUrl: process.env.PROTRACTOR_ENDPOINT || 'http://localhost:1616',
+    //baseUrl: 'http://realstaging.lergodev.info',
 
 
-  jasmineNodeOpts: {
-    onComplete: null,
-    isVerbose: true,
-    showColors: true,
-    includeStackTrace: true,
-    defaultTimeoutInterval: 40000
-  }
+    jasmineNodeOpts: {
+        onComplete: null,
+        isVerbose: true,
+        showColors: true,
+        includeStackTrace: true,
+        defaultTimeoutInterval: 360000 // using browserstack tests will be slow.. give them 6 minutes timeout
+    }
 };
