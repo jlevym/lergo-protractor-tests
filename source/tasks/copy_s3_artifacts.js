293572c0214f09007702ad04f8aed4138ab544b8
Guy Mograbi <guy.mograbi@gmail.com>
2015-12-05 09:24:25 +0200

    allow to define aws.json with environment variable

--- source/tasks/copy_s3_artifacts.js
+++ source/tasks/copy_s3_artifacts.js
@@ -2,7 +2,7 @@
 var AWS = require('aws-sdk');
 var _ = require('lodash');
 var path = require('path');
-var awsDetails = require(path.resolve(__dirname, '/vagrant/dev/aws.json'));
+var awsDetails = require(process.env.AWS_JSON || path.resolve(__dirname, '/vagrant/dev/aws.json'));
 
 var logger = require('log4js').getLogger('copy_s3_artifacts');
 var s3 = new AWS.S3({ accessKeyId: awsDetails.accessKey, secretAccessKey : awsDetails.secretAccessKey });
@@ -23,7 +23,7 @@ _.each(files, function(f){
     s3.copyObject({
         Bucket: awsDetails.bucket,
         CopySource: 'lergo-backups/artifacts/build-lergo-' + promoteBuild+ '/jobs/build-lergo/' + promoteBuild + '/' + f,
-        Key : 'artifacts/latest/' + f,
+        Key : 'artifacts/latest-test/' + f,
         ACL: 'public-read'
 
     }, function(err, data) {
