ec58d17a22b76dbf8c5e307de2fc74d2d8058e04
Mukta Garodia <mukta.garodia@gmail.com>
2015-12-13 09:36:29 +0200

    add option to promote build to production

--- source/tasks/copy_s3_artifacts.js
+++ source/tasks/copy_s3_artifacts.js
@@ -9,29 +9,78 @@ var s3 = new AWS.S3({ accessKeyId: awsDetails.accessKey, secretAccessKey : awsDe
 
 var promoteBuild = process.env.PROMOTE_BUILD_NUMBER;
 
+var PROMOTE_TYPES={ STAGING : 'staging', PRODUCTION : 'production'};
+
+var promoteType = process.env.PROMOTE_TYPE || 'staging'; //staging / production
+
 if ( !promoteBuild ){
     logger.error('PROMOTE_BUILD_NUMBER is missing');
     process.exit(1);
 }
 
 logger.info('PROMOTE_BUILD_NUMBER is ' + promoteBuild);
-
+logger.info('PROMOTE_TYPE is', promoteType);
 var files = ['build.id','install.sh','lergo-ri-0.0.0.tgz','lergo-ui-0.0.0.tgz'];
 
 
-_.each(files, function(f){
-    s3.copyObject({
-        Bucket: awsDetails.bucket,
-        CopySource: 'lergo-backups/artifacts/build-lergo-' + promoteBuild+ '/jobs/build-lergo/' + promoteBuild + '/' + f,
-        Key : 'artifacts/latest/' + f,
-        ACL: 'public-read'
+if ( promoteType === PROMOTE_TYPES.STAGING ) {
 
-    }, function(err, data) {
-        if ( err ){ logger.error(err); }
-        if ( data ) { logger.info(data); }
+    _.each(files, function (f) {
+        s3.copyObject({
+            Bucket: awsDetails.bucket,
+            CopySource: 'lergo-backups/artifacts/build-lergo-' + promoteBuild + '/jobs/build-lergo/' + promoteBuild + '/' + f,
+            Key: 'artifacts/latest/' + f,
+            ACL: 'public-read'
+
+        }, function (err, data) {
+            if (err) {
+                logger.error(err);
+            }
+            if (data) {
+                logger.info(data);
+            }
+
+        });
 
     });
+}
 
-});
+if ( promoteType === PROMOTE_TYPES.PRODUCTION ){
+    _.each(files, function (f) {
+        s3.copyObject({
+            Bucket: awsDetails.bucket,
+            CopySource: 'lergo-backups/artifacts/build-lergo-' + promoteBuild + '/jobs/build-lergo/' + promoteBuild + '/' + f,
+            Key: 'lergo-backups/builds/production/' + promoteBuild + '/' + f,
+            ACL: 'public-read'
+
+        }, function (err, data) {
+            if (err) {
+                logger.error(err);
+            }
+            if (data) {
+                logger.info(data);
+            }
+
+        });
+
+        s3.copyObject({
+            Bucket: awsDetails.bucket,
+            CopySource: 'lergo-backups/artifacts/build-lergo-' + promoteBuild + '/jobs/build-lergo/' + promoteBuild + '/' + f,
+            Key: 'lergo-backups/builds/production/latest/' +  f,
+            ACL: 'public-read'
+
+        }, function (err, data) {
+            if (err) {
+                logger.error(err);
+            }
+            if (data) {
+                logger.info(data);
+            }
+
+        });
+
+
+    });
+}
 
 
